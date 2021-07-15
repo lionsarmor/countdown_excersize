a82870070b3bb025a66709433cec46d6f8b066ab
Roddy <james.m.weeks@gmail.com>
2021-07-14 20:52:16 -0600

    boilerplate setup, VUE3, VITE, TAILWIND

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Countdown />
+</template>
+
+<script>
+import Countdown from './components/Countdown.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Countdown
+  }
+}
+</script>
---
+++ src/components/Countdown.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="grid grid-cols-1">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Countdown',
+  props: {
+    
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
