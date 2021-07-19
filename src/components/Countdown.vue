1f71ca3c983e81eeba8accc46476e2c7bf2f70a5
Roddy <james.m.weeks@gmail.com>
2021-07-18 22:12:11 -0600

    added blinking animation

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -15,21 +15,21 @@
             </div>
         </section>
         <section>
-            <a class="inline-block ml-6 text-9xl">{{ display_min }} : {{ display_sec }}</a>
+            <a :id="blink" :style="{ color: activeColor }" class="inline-block ml-6 text-9xl">{{ display_min }} : {{ display_sec }}</a>
             <div class="inline-block">
-                <button v-show="paused == false" @click="paused = true" class="absolute top-40 text-black py-2 px-4">
+                <button v-show="paused == false && start == true" @click="paused = true" class="absolute top-40 text-black py-2 px-4">
                     <img id="play" width="62" src="src/assets/pause.png" alt="Play Button" />
                 </button>
-                <button v-show="paused == true" @click="paused = false, min_to_seconds()" class="absolute top-40 text-black py-2 px-4">
+                <button v-show="paused == true && start == true" @click="paused = false, min_to_seconds()" class="absolute top-40 text-black py-2 px-4">
                     <img id="play" width="62" src="src/assets/play.png" alt="Play Button" />
                 </button>
             </div>
         </section>
         <section>
             <div class="p-2 my-4">
-                <div class="inline-block mx-5"><button @click="speed_adjust(0)" class="bg-white-500 border-2 active:bg-gray-400 text-black font-bold py-2 px-4 w-24 h-12">1X</button></div>
-                <div class="inline-block mx-5"><button @click="speed_adjust(1.5)" class="bg-white-500 border-2 active:bg-gray-400 text-black font-bold py-2 px-4 w-24 h-12">1.5X</button></div>
-                <div class="inline-block mx-5"><button @click="speed_adjust(2)" class="bg-white-500 border-2 active:bg-gray-400 text-black font-bold py-2 px-4 w-24 h-12">2X</button></div>
+                <div class="inline-block mx-5"><button :style="{ backgroundColor: button[0] }" @click="speed_adjust(0)" class="bg-white-500 border-2 active:bg-gray-400 text-black font-bold py-2 px-4 w-24 h-12">1X</button></div>
+                <div class="inline-block mx-5"><button :style="{ backgroundColor: button[1] }" @click="speed_adjust(1.5)" class="bg-white-500 border-2 active:bg-gray-400 text-black font-bold py-2 px-4 w-24 h-12">1.5X</button></div>
+                <div class="inline-block mx-5"><button :style="{ backgroundColor: button[2] }" @click="speed_adjust(2)" class="bg-white-500 border-2 active:bg-gray-400 text-black font-bold py-2 px-4 w-24 h-12">2X</button></div>
             </div>
         </section>
     </div>
@@ -49,6 +49,9 @@
                 speed: 1000,
                 paused: false,
                 start: false,
+                button: [null, null, null],
+                activeColor: "black",
+                blink: "none",
             };
         },
         methods: {
@@ -66,10 +69,19 @@
                 this.paused = false;
                 if (adjust == 1.5) {
                     this.speed = 750;
+                    this.button[1] = "#4fd499";
+                    this.button[2] = null;
+                    this.button[0] = null;
                 } else if (adjust == 2) {
                     this.speed = 500;
+                    this.button[2] = "#4fd499";
+                    this.button[0] = null;
+                    this.button[1] = null;
                 } else {
                     this.speed = 1000;
+                    this.button[0] = "#4fd499";
+                    this.button[1] = null;
+                    this.button[2] = null;
                 }
             },
             timer() {
@@ -79,6 +91,8 @@
                 this.display_min = String(minutes).padStart(2, "0");
                 this.display_sec = String(seconds).padStart(2, "0");
                 if (this.count <= 0) {
+                    this.countDownMessage = "Stopped!";
+                    this.start = "false";
                     return;
                 }
                 let timeout = setTimeout(this.timer, this.speed);
@@ -87,7 +101,9 @@
                 } else if (this.count == (this.user_input_min * 60) / 2) {
                     this.countDownMessage = "More then halfway there!";
                 } else if (this.count == 20) {
-                    this.countDownMessage = "20";
+                    this.activeColor = "red";
+                } else if (this.count <= 10) {
+                    this.blink = "time_text";
                 }
             },
         },
